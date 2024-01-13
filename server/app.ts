3514096f86bdcd91c7f780ba564553234811765a
Romain Séguy <rom.seguy@gmail.com>
2024-01-13 12:42:31 +0100

    1

--- server/app.ts
+++ server/app.ts
@@ -1,15 +1,73 @@
-import express, { Router } from 'express';
-class App {
+import axios from "axios";
+import express, { Request, Router } from "express";
+import https from "https";
 
-    public router: Router = express.Router();
+const agent = new https.Agent({
+  rejectUnauthorized: false,
+  requestCert: false
+});
 
-    constructor() {
-        this.router.get('/', (req, res) => {
-            res.send("Welcome to the API!")
-        })
-    }
+const client = axios.create({
+  responseType: "json",
+  withCredentials: true,
+  httpsAgent: agent,
+  timeout: 60000
+});
+
+interface RequestParams {}
+
+interface ResponseBody {}
+
+interface RequestBody {}
+
+interface RequestQuery {
+  url?: string;
 }
 
-const api = new App()
+class App {
+  public router: Router = express.Router();
+
+  constructor() {
+    this.router.get("/", (req, res) => {
+      res.send("Welcome to the API!");
+    });
+
+    this.router.get(
+      "/fetch",
+      async (
+        req: Request<RequestParams, ResponseBody, RequestBody, RequestQuery>,
+        res
+      ) => {
+        try {
+          let { query } = req;
+
+          if (!query.url) throw new Error("NO URL");
+
+          let url = `https://api.allorigins.win/get?url=${encodeURIComponent(
+            query.url.split("#")[0]
+          )}`;
+
+          const { data } = await client.get(url);
+          console.log("🚀 ~ /fetch ~ data:", data);
+
+          if (!data.contents) throw new Error("NO CONTENTS");
+
+          const title = data.contents.split("<title>")[1].split("</title>")[0];
+
+          res.status(data.status.http_code).json({ title });
+          //res.status(200).json({});
+        } catch (error: any) {
+          console.log("🚀 ~ /fetch ~ error:", error.message);
+
+          res
+            .status(200)
+            .json({ code: error.response ? error.response.status : 200 });
+        }
+      }
+    );
+  }
+}
+
+const api = new App();
 
 export default api;
