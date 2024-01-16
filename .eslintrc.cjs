b7ae4353844a09b86bdc5ab363434a057ffebe93
Romain Séguy <rom.seguy@gmail.com>
2024-01-16 08:50:56 +0100

    eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,6 +15,8 @@ module.exports = {
       { allowConstantExport: true },
     ],
     "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/triple-slash-reference": "off"
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/triple-slash-reference": "off",
+    "prefer-const": "off",
   },
 }
--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,22 +1,20 @@
-import React from 'react';
-import ReactDomServer from 'react-dom/server'
-import { StaticRouter } from 'react-router-dom/server'
-import App from './App';
+import React from "react";
+import ReactDomServer from "react-dom/server";
+import { StaticRouter } from "react-router-dom/server";
+import App from "./App";
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const render = (url: string, _context: any) => {
+  const html = ReactDomServer.renderToString(
+    <StaticRouter location={url}>
+      <App />
+    </StaticRouter>
+  );
 
-    const html = ReactDomServer.renderToString(
-        <StaticRouter location={url}>
-            <App />
-        </StaticRouter>
-    )
-
-    return { html }
-}
+  return { html };
+};
 
 const _export = {
-    render
-}
+  render
+};
 
-export default _export
+export default _export;
