448a76fd90ff62633e735d501be8ba51d1d8b1bb
Romain Séguy <rom.seguy@gmail.com>
2024-04-10 22:21:36 +0200

    extract-date

---
+++ src/lib/extract-date/createMovingChunks.ts
@@ -0,0 +1,17 @@
+export default (haystack, sliceLength: number) => {
+  const slices = [];
+
+  let index = 0;
+
+  while (index < haystack.length) {
+    const result = haystack.slice(index, index + sliceLength);
+
+    if (result.length === sliceLength) {
+      slices.push(result);
+    }
+
+    index++;
+  }
+
+  return slices;
+};
---
+++ src/lib/extract-date/types.ts
@@ -0,0 +1,21 @@
+export type DateMatchType = {
+  date: string;
+};
+
+export type DirectionType = "DM" | "DMY" | "DYM" | "MD" | "YDM" | "YMD" | "MDY";
+
+export type UserConfigurationType = {
+  direction?: DirectionType;
+  locale?: string;
+  maximumAge?: number;
+  minimumAge?: number;
+  timezone?: string;
+};
+
+export type ConfigurationType = {
+  direction?: DirectionType;
+  locale?: string;
+  maximumAge: number;
+  minimumAge: number;
+  timezone?: string;
+};
